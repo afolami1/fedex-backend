8ebb94100806686982dd61b0654b6b095baae561
afolabi <afordlyrical@gmail.com>
2026-09-11 04:41:24 -0500

    Fix admin account creation

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
+
 python manage.py migrate
-python manage.py createsuperuser --noinput || true
+
+python manage.py shell -c "import os; from django.contrib.auth import get_user_model; User=get_user_model(); u,created=User.objects.get_or_create(username=os.environ['DJANGO_SUPERUSER_USERNAME'], defaults={'email':os.environ.get('DJANGO_SUPERUSER_EMAIL','')}); u.email=os.environ.get('DJANGO_SUPERUSER_EMAIL',''); u.is_staff=True; u.is_superuser=True; u.set_password(os.environ['DJANGO_SUPERUSER_PASSWORD']); u.save(); print('ADMIN ACCOUNT READY:',u.username)"
